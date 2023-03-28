4662895c7940e3f6324c2063ec48c0a253a65f71
fred077a <fred18q3@stud.kea.dk>
2023-03-28 09:20:12 +0200

    homePage template

--- src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
+++ src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
@@ -3,6 +3,7 @@ package com.kodeklubben.miniprojekt.controllers;
 import com.kodeklubben.miniprojekt.repositories.WishListRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class Controller {
     }
 
     @GetMapping("/")
-    public ResponseEntity<ArrayList<String>> getWishList() {
-        return new ResponseEntity<>(wishListRepository.getAllUser(1), HttpStatus.OK);
+    public String getAllSuperheroes() {
+        return "homePage";
     }
 }

a24605b8c49e4624b6111ce887090efa62568798
danieljappe <danieljappe@icloud.com>
2023-03-28 12:28:09 +0200

    ctrler update

--- src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
+++ src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
@@ -1,14 +1,14 @@
 package com.kodeklubben.miniprojekt.controllers;
 
+import com.kodeklubben.miniprojekt.models.UserModel;
 import com.kodeklubben.miniprojekt.models.WishListModel;
 import com.kodeklubben.miniprojekt.repositories.WishListRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 @org.springframework.stereotype.Controller
@@ -22,7 +22,41 @@ public class Controller {
     }
 
     @GetMapping("/")
-    public String getAllSuperheroes() {
-        return "homePage";
+    public String homePage(){
+        return "index";
     }
+
+    // Login
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        UserModel userModel = new UserModel();
+        model.addAttribute("usermodel", userModel);
+        return "LoginPage";
+    }
+
+    @PostMapping("/login")
+    public String submitLogin(@ModelAttribute("userModel") UserModel userModel) {
+        wishListRepository.getIdFromAuthentication(userModel.getEmail(), userModel.getPassword());
+        return "userPage";
+    }
+
+
+    // Create User
+
+    @GetMapping("/createuser")
+    public String createUser(Model model) {
+        UserModel userModel = new UserModel();
+        model.addAttribute("usermodel", userModel);
+        return "RegistrationPage";
+    }
+
+    @PostMapping("/createUser")
+    public String submitUser(@ModelAttribute("userModel") UserModel userModel) throws SQLException {
+        wishListRepository.insertNewUser(userModel.getName(), userModel.getEmail(), userModel.getPassword());
+        return "userPage";
+    }
+
+
+
 }

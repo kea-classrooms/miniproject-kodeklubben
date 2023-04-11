844b83cc537949b508d7aadd189363745ff851fc
danieljappe <danieljappe@icloud.com>
2023-04-11 13:55:36 +0200

    profile.html forms

--- src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
+++ src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
@@ -58,6 +58,8 @@ public class Controller {
     @PostMapping("/createWishList")
     public String submitCreateWishlist(@ModelAttribute("wishListModel") WishListModel wishListModel, UserModel userModel, Model model) {
         System.out.println(wishListModel);
+        System.out.println(userModel);
+        System.out.println(model);
         wishListRepository.insertNewWishList(wishListModel.getListName(), wishListRepository.getIdFromAuthentication(userModel.getEmail(), userModel.getPassword()));
         int userId = wishListRepository.getIdFromAuthentication(userModel.getEmail(), userModel.getPassword());
         ArrayList<WishListModel> wishLists = wishListRepository.getWishLists(userId);

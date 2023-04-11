80ca66d78343514df20e48cbb772e86544e84aaf
fred077a <fred18q3@stud.kea.dk>
2023-04-11 14:13:35 +0200

    inden jeg smutter

--- src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
+++ src/main/java/com/kodeklubben/miniprojekt/controllers/Controller.java
@@ -66,8 +66,6 @@ public class Controller {
         return "profile";
     }
 
-
-
     //login with email and password
     @GetMapping("/credentials")
     public String submitLogin(@RequestParam String id, Model model, boolean isLogin) {
